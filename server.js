ebb4914a726755e171da2545683dacc58077a699
Emmanouil Tsikniadopoulos <manos.tsik7@gmail.com>
2019-04-10 02:39:14 +0300

    Added a Procfile

--- server.js
+++ server.js
@@ -5,32 +5,12 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/css/styles.css", function(req, res) {
-  res.sendFile(__dirname + "/css/styles.css");
-});
-
-app.get("/js/utility.js", function(req, res) {
-  res.sendFile(__dirname + "/js/utility.js");
-});
-
-app.get("/js/scrollspy.js", function(req, res) {
-  res.sendFile(__dirname + "/js/scrollspy.js");
-});
-
-app.get("/assets/image-profile.png", function(req, res) {
-  res.sendFile(__dirname + "/assets/image-profile.png");
-});
-
-app.get("/assets/trophy.png", function(req, res) {
-  res.sendFile(__dirname + "/assets/trophy.png");
-});
-
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server listen in 3000 port");
 });
-//app.listen(3000, "192.168.1.14");
