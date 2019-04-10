56592666e941428064181ec41d115c6d83510ad8
Emmanouil Tsikniadopoulos <manos.tsik7@gmail.com>
2019-04-10 16:21:04 +0300

    Add ejs engine

--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const app = express();
 
+//Ejs engine
+app.set('view engine', 'ejs');
+//Body parser url encoded data
 app.use(bodyParser.urlencoded({extended: true}));
+//Add static urls implementation
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
+  var personName = "Emmanouil Tsikniadopoulos";
+
+  res.render('index', {personName: personName})
 });
 
 app.listen(process.env.PORT || 3000, function() {
